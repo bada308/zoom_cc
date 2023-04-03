3422a8a634773c389c8bd4682d938751caaf4a4e
강바다 <bada@gangbadaui-MacBookAir.local>
2023-04-03 10:47:00 +0900

    feat : room 기능 추가, 참가 시 Message 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,35 @@
-const msgList = document.querySelector("ul");
-const msgForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nickname");
+const frontSocket = io();
 
-const frontSocket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.querySelector("#welcome");
+const welForm = welcome.querySelector("form");
+const room = document.querySelector("#room");
 
-// type과 값을 저장한 객체를 stringify해서 return
-const makeMessage = (type, payload) => {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+// 방에 참가하기 전에 room div 숨기기
+room.hidden = true;
+let roomName;
+
+const addMessage = (message) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
 };
-// socket open event
-frontSocket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
 
-// 메세지 받기 event
-frontSocket.addEventListener("message", (message) => {
-  const newMsg = document.createElement("li");
-  newMsg.innerText = message.data;
-  msgList.appendChild(newMsg);
-});
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room : ${roomName}`;
+};
 
-// socket close event
-frontSocket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
-
-// message submit
-const handleSubmit = (event) => {
+welForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = msgForm.querySelector("input");
-  frontSocket.send(makeMessage("new_msg", input.value));
+  const input = welForm.querySelector("input");
+  roomName = input.value;
+  frontSocket.emit("enter_room", input.value, showRoom);
   input.value = "";
-};
+});
 
-// nickname submit
-const handleNickSubmit = (event) => {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  frontSocket.send(makeMessage("nickname", input.value));
-  input.value = "";
-};
-
-msgForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+frontSocket.on("welcome", () => {
+  addMessage("Someone joined!");
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import WebSocket from "ws";
+import { Server } from "socket.io";
 import http from "http"; // node.js에 이미 설치되어 있기 때문에 따로 설치할 필요 없음
 
 const app = express();
@@ -10,13 +10,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 // http 서버 생성
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+// io 서버 생성
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event : ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome"); // 왜 나는 나한테도 message가 보내졌지?
+  });
+});
+
+/*
 // ws 서버 생성
 const wss = new WebSocket.Server({ server });
-
 // 연결된 socket 저장
 const sockets = [];
 
@@ -41,5 +53,6 @@ wss.on("connection", (socket) => {
     }
   });
 });
-
-server.listen(3000, handleListen);
+*/
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
