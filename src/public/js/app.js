007bae539fb5d0befe12860e4fa1ef6320835ea7
bada308 <203190@jnu.ac.kr>
2023-04-30 22:22:03 +0900

    feat : 3.7 IceCandidate까지 기능추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,17 @@ const myFace = document.querySelector("#myFace");
 const muteBtn = document.querySelector("#mute");
 const cameraBtn = document.querySelector("#camera");
 const cameraSelect = document.querySelector("#cameras");
+const call = document.querySelector("#call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName = "";
+let myPeerConnection;
+
+// media
 
 async function getCamera() {
   try {
@@ -87,3 +94,73 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleClickMute);
 cameraBtn.addEventListener("click", handleClickCamera);
 cameraSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form (join a room)
+
+const welcome = document.querySelector("#welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+async function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  roomName = input.value;
+  await initCall();
+  frontSocket.emit("join_room", roomName);
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+frontSocket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  frontSocket.emit("offer", offer, roomName);
+  console.log("sent the offer");
+});
+
+frontSocket.on("offer", async (offer) => {
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  frontSocket.emit("answer", answer, roomName);
+  console.log("sent the answer");
+});
+
+frontSocket.on("answer", (answer) => {
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+frontSocket.on("ice", (ice) => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC Code
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log("sent the candidate");
+  frontSocket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.querySelector("#peerFace");
+  peerFace.srcObject = data.stream;
+}
